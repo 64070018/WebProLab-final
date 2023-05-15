f2d1308051217d373936414b040e9ccd8ab1c445
Chanakarn <88476014+64070018@users.noreply.github.com>
2023-05-15 11:45:52 +0700

    Add files via upload

---
+++ ของพี่ที่เฉลยในห้อง.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const app = express();
+
+const Joi = require("joi")
+const pool = require('./config/database');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ *  เริ่มทำข้อสอบได้ที่ใต้ข้อความนี้เลยครับ
+ * !!! ไม่ต้องใส่ app.listen() ในไฟล์นี้นะครับ มันจะไป listen ที่ไฟล์ server.js เองครับ !!!
+ * !!! ห้ามลบ module.exports = app; ออกนะครับ  ไม่งั้นระบบตรวจไม่ได้ครับ !!!
+*/
+
+// tip
+
+
+// '/:params/' => req.params.name
+// '/?string/' => req.query.name
+
+
+// update put
+// create post
+// delete delete
+// select get data ไม่จำเป็นต้องทำ transection
+app.get('/get_todo', async (req, res) => {
+    const [todo] = await pool.query('select * from todo')
+
+    const data = { name: 'fern' }
+    const { name } = data // fern
+    function fern() {
+        return new Promise()
+    }
+    function fern2() {
+        return "fern"
+    }
+
+    // ดักาค่า null []
+
+    let array = []
+    let nulVal = null // false
+    if (!nulVal) {
+        // do sth
+        res.send("nulVar is null")
+    }
+
+    if (!array.length || array.length == 0) {
+        res.send("array is null")
+    }
+    const asdf = await fern()
+    const qwer = await fern2() // ไม่จำเป็นต้อง await
+    return res.send(data)  // will send only round so put after return
+
+})
+
+
+// update put
+// create post
+// delete delete
+// select get data ไม่จำเป็นต้องทำ transection
+
+// ข้อ 2 delete
+app.delete('/todo/:id', async (req, res) => {
+    // '/:params/' => req.params.name
+    // '/?string/' => req.query.name
+    const id = req.params.id
+    const [todo] = await pool.query('select * from todo where id = ?', [id]) // array
+
+    if (todo.length == 0) {
+        return res.status(404).send({
+            "message": "ไม่พบ ToDo ที่ต้องการลบ"
+        })
+    }
+
+
+    // transection
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+    try {
+        // pool ?
+        const [data] = await pool.query('delete from todo where id = ?', [id])
+        await conn.commit()
+        res.status(200).send({
+            "message": `ลบ ToDo '${todo[0].title}' สำเร็จ`,
+        })
+    } catch (err) {
+        console.log(err)
+        conn.rollback()
+        res.status(404).send({
+            "message": "ไม่พบ ToDo ที่ต้องการลบ"
+        })
+
+    } finally {
+        conn.release() // don't forget
+    }
+})
+
+
+
+
+// ข้อ 3
+// then เมื่อ start daate เป็น date ทำต่อจาก is
+const check_date = Joi.object({
+    start_date: Joi.date().iso().required(),
+    end_date: Joi.date().iso().required().when('start_date', {
+        is: Joi.date().required(),
+        then: Joi.date().min(Joi.ref('start_date')).required()
+    })
+})
+
+
+app.get('/todo', async (req, res) => {
+    console.log("enter")
+    try {
+        // await check_date.validate(req.query, { abortEarly: false })
+        await check_date.validateAsync(req.query, { abortEarly: false })
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json(err)
+    }
+
+    const start_date = req.query.start_date
+    const end_date = req.query.end_date
+    console.log(start_date)
+    console.log(end_date)
+    const [todo] = await pool.query(`select *, DATE_FORMAT(due_date, '%Y-%m-%d') AS due_date from todo where due_date between ? and ?`, [start_date, end_date])
+
+    res.status(200).send(todo)
+
+})
+
+
+
+
+
+
+
+module.exports = app;
---
+++ ลองทำเองเทสผ่านแล้ว.js
@@ -0,0 +1,157 @@
+const express = require('express');
+const app = express();
+
+const Joi = require("joi")
+const pool = require('./config/database');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ *  เริ่มทำข้อสอบได้ที่ใต้ข้อความนี้เลยครับ
+ * !!! ไม่ต้องใส่ app.listen() ในไฟล์นี้นะครับ มันจะไป listen ที่ไฟล์ server.js เองครับ !!!
+ * !!! ห้ามลบ module.exports = app; ออกนะครับ  ไม่งั้นระบบตรวจไม่ได้ครับ !!!
+*/
+
+
+// update table set attr = value where condition
+// insert into table(attr) values(),();
+// delete from table where condition;
+// select * from join using on connect where condition;
+
+// อย่าลืม res.send()
+
+// ดูชื่อคอลัมดีๆ บางทีมันเป้นคำสั่ง order by or group from brabrabra
+
+// date.now() => ออกเป็นตัวเลข
+// new Date() => console.log(date); // Fri Jun 17 2022 11:27:28 GMT+0100 (British Summer Time)
+// DATE_FORMAT(check_in, '%Y-%m-%d') as check_in
+
+
+// Joi
+//     try {
+//         // await check_date.validate(req.query, { abortEarly: false })
+//         await check_date.validateAsync(req.query, { abortEarly: false })
+//     } catch (err) {
+//         console.log(err)
+//         return res.status(400).json(err)
+//     }
+
+
+// const check_date = Joi.object({
+//     start_date: Joi.date().iso().required(),
+//     end_date: Joi.date().iso().required().when('start_date', {
+//         is: Joi.date().required(),
+//         then: Joi.date().min(Joi.ref('start_date')).required()
+//     })
+// })
+const check_require = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    due_date: Joi.date()
+})
+app.post('/todo', async (req, res) => {
+    try {
+        await check_require.validateAsync(req.body, { abortEarly: false })
+    } catch (err) {
+        console.log(err)
+        if (!req.body.title) {
+            return res.status(400).send({
+                "message": "ต้องกรอก title"
+            })
+        }
+        if (!req.body.description) {
+            return res.status(400).send({
+                "message": "ต้องกรอก description"
+            })
+        }
+        // return res.status(400).send(err)
+    }
+
+    const title = req.body.title
+    const des = req.body.description
+
+    let due_date = req.body.due_date
+    const [order] = await pool.query('select MAX(t.order) as der from todo t')
+
+    let der = order[0].der + 1
+
+    if (!due_date) {
+        due_date = new Date()
+        console.log(due_date)
+    } else {
+        due_date = req.body.due_date
+    }
+
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+    try {
+        const [todo] = await conn.query('insert into todo(title, description,due_date, `order`) values(?,?,?, ?)', [title, des, due_date, der])
+        const [getDate] = await conn.query(' SELECT *, DATE_FORMAT(due_date, "%Y-%m-%d") AS due_date FROM todo where id = ?', [todo.insertId])
+
+        conn.commit()
+        res.status(201).send({
+            "message": `สร้าง ToDo '${title}' สำเร็จ`,
+            "todo": {
+                "id": todo.insertId,
+                "title": title,
+                "description": des,
+                "due_date": getDate[0].due_date,
+                "order": der
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    } finally {
+        conn.release()
+    }
+})
+
+app.delete('/todo/:id', async (req, res) => {
+    const [todo] = await pool.query('select title from todo where id = ?', [req.params.id])
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+    try {
+        await conn.query('delete from todo where id =?', [req.params.id])
+        conn.commit()
+        res.status(200).send({
+            "message": `ลบ ToDo '${todo[0].title}' สำเร็จ`,
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(404).send({
+            "message": "ไม่พบ ToDo ที่ต้องการลบ"
+        })
+    } finally {
+        conn.release()
+    }
+})
+
+// then เมื่อ start daate เป็น date ทำต่อจาก is
+const check_date = Joi.object({
+    start_date: Joi.date().iso().required(),
+    end_date: Joi.date().iso().required().when('start_date', {
+        is: Joi.date().required(),
+        then: Joi.date().min(Joi.ref('start_date')).required()
+    })
+})
+
+app.get('/todo', async (req, res, next) => {
+    const start_date = req.query.start_date
+    const end_date = req.query.end_date
+    if (start_date || end_date) {
+        const [todo] = await pool.query(`
+    select *, DATE_FORMAT(due_date, "%Y-%m-%d") AS due_date
+    from todo
+    where due_date between ? and ?`, [req.query.start_date, req.query.end_date])
+        res.status(200).send(todo)
+    }
+    else {
+        const [todo] = await pool.query(`
+    select *, DATE_FORMAT(due_date, "%Y-%m-%d") AS due_date
+    from todo`, [req.query.start_date, req.query.end_date])
+        res.status(200).send(todo)
+
+    }
+})
+module.exports = app;
